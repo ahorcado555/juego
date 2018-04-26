7e06096c07659f43914bc13e703f5da9677a2326
gcanasto <gcanasto@solati.com.co>
2018-04-26 12:23:31 -0500

    tres primeras pruebas unitarias

--- spec/TriquiSpec.php
+++ spec/TriquiSpec.php
@@ -12,4 +12,19 @@ class TriquiSpec extends ObjectBehavior
     {
         $this->shouldHaveType(Triqui::class);
     }
+   // J1 marca poscion 5 
+    function it_j1_marca_posicion_5()
+    {
+    $this->setJugada(1,5)->shouldReturn('Turno J2');
+    }
+    function it_j2_marca_posicion_3()
+    {
+    $this->setJugada(2,3)->shouldReturn('Turno J1');
+    }
+
+    function it_j1yj2_marca_posicion_5()
+    {
+    $this->setJugada(1,5);
+    $this->setJugada(2,5)->shouldReturn('Posicion ya Jugada, marque otra posicion');
+    }
 }
--- src/Triqui.php
+++ src/Triqui.php
@@ -2,4 +2,28 @@
 
 class Triqui
 {
+    private $jugadores = array('1' => 'J1', '2' => 'J2');
+    private $jugadoresInv = array('1' => '2', '2' => '1');
+    private  $posiciones =  array();
+
+    public function setJugada($jugador, $posicion)
+    {
+        if ($this->setPosicion($posicion)){
+            $jugadorSiguiente =  $this->siguienteJugador($jugador);
+            return  'Turno '. $this->jugadores[$jugadorSiguiente];    
+        }
+        else
+          return  'Posicion ya Jugada, marque otra posicion';
+        
+    }
+    private function  siguienteJugador($jugador) 
+    {
+        return $this->jugadoresInv[$jugador];
+    }
+    private function setPosicion($posicion) {
+        if(!in_array($posicion, $this->posiciones))
+            return  array_push($this->posiciones, $posicion);
+        else
+            return false;
+    }
 }
